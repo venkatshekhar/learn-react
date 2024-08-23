8377750193e7f047c3f804e2bcb746345d884cbe
Shekhar <pc@192.168.1.5>
2024-08-23 08:12:13 +0530

    added Nested element

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
-const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World from React!");
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement(
+      "h1",
+      { id: "heading", xyz: "abc" },
+      "Hello World from React!"
+    )
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
+root.render(parent);
