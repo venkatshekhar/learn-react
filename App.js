2fb5a3ecf8bfa935b7a117335e4b98ab412ec93b
Shekhar <pc@192.168.1.5>
2024-08-23 08:04:11 +0530

    split script from html and adding prop in React Element

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
