06da911eccdefcfa32dfe0c69a56b8e79667607d
Shekhar <pc@192.168.1.5>
2024-08-23 08:17:43 +0530

    adding array of element in children

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement(
-      "h1",
-      { id: "heading", xyz: "abc" },
-      "Hello World from React!"
-    )
-  )
+  [React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading", xyz: "abc" }, "Welcome"),
+    React.createElement("h2", {}, "Hello World from React!"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading", xyz: "abc" }, "Welcome"),
+    React.createElement("h2", {}, "Hello World from React!"),
+  ])]
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
